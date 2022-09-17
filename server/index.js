61ddff9850b1ad44d3e5bc14458a67b00be25688
Nabeel <43858092+nabeelkv@users.noreply.github.com>
2022-09-17 19:07:26 +0530

    added models, routes, controllers

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes); //added prefix for all posts.js routes for posts
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); //to receive request
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //to send request
+app.use(cors());
+
+//Datbase setup (MangoDB Atlas Cluster - Online Cloud Database)
+const CONNECTION_URL = 'mongodb+srv://nabeel:nabeel123@cluster0.ma6in1j.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
