4816c18ab18dd6da808622cf9a2f9de703b2f8b0
Nabeel <43858092+nabeelkv@users.noreply.github.com>
2022-09-23 00:27:20 +0530

    added redux,thunk,toolkit

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes); //added prefix for all posts.js routes for posts
-
 app.use(bodyParser.json({ limit: "30mb", extended: true })); //to receive request
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //to send request
 app.use(cors());
+
+app.use('/posts', postRoutes); //added prefix for all posts.js routes for posts
 
 //Datbase setup (MangoDB Atlas Cluster - Online Cloud Database)
 const CONNECTION_URL = 'mongodb+srv://nabeel:nabeel123@cluster0.ma6in1j.mongodb.net/?retryWrites=true&w=majority';
