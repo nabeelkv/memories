9bd6bf7860db8ffa1986343b4889315439b92615
Nabeel Muhammed <43858092+nabeelkv@users.noreply.github.com>
2022-10-02 00:09:16 +0530

    removed bodyParser module

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
@@ -7,8 +6,8 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true })); //to receive request
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //to send request
+app.use(express.json({ limit: "30mb", extended: true })); //to receive request
+app.use(express.urlencoded({ limit: "30mb", extended: true })); //to send request
 app.use(cors());
 
 app.use('/posts', postRoutes); //added prefix for all posts.js routes for posts
